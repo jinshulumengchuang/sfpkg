cb86e7260c4714d67b72ff611ca1439ef8dd1807
jinshulumengchuang <1320553934@qq.com>
2016-02-16 15:22:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "The program depends on gksudo and squashfs-tools"
 echo 'Installing......................'
-install -m755 mksfpkg exsfpkg root.sh /usr/bin/
+install -m755 mksfpkg exsfpkg sfroot /usr/bin/
 echo 'done'
 exit
