0c70ec0cfa195603c79017d8637a63c2b063e593
Yu Haixin <836401406@qq.com>
2016-02-22 18:18:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /tmp
 echo "Downloading....Please wait a minute"
-wget https://raw.githubusercontent.com/jinshulumengchuang/sfpkg/master/exsfpkg
-wget https://raw.githubusercontent.com/jinshulumengchuang/sfpkg/master/mksfpkg
-wget https://raw.githubusercontent.com/jinshulumengchuang/sfpkg/master/sfroot
+wget https://raw.githubusercontent.com/name1e5s/sfpkg/master/exsfpkg
+wget https://raw.githubusercontent.com/name1e5s/sfpkg/master/mksfpkg
+wget https://raw.githubusercontent.com/name1e5s/sfpkg/master/sfroot
 echo "The program depends on gksudo and squashfs-tools"
 echo 'Installing......................'
 install -m755 mksfpkg exsfpkg sfroot /usr/bin/
